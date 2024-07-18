dba4b8deb01372c71def220320ae3026d081340f
tingyixuan <yixuan@9loop.asia>
2024-07-18 11:49:50 +0800

    FrontEnd Done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let power: 'on' | 'off' = 'off';
+	let mode: 'cool' | 'heat' | 'vent' | 'dry' | undefined;
+	let speed: 'low' | 'medium' | 'high' | 'auto' | undefined;
+	let temp: number | undefined;
+
+	const switch_ = () => {
+		if ((power = power === 'off' ? 'on' : 'off') === 'on') {
+			mode = mode || 'cool';
+			speed = speed || 'auto';
+			temp = temp || 23;
+		}
+	};
+
+	const cycle = <T>(arr: T[], current: T): T => arr[(arr.indexOf(current) + 1) % arr.length];
+
+	const mode_ = () => mode = power === 'on' ? cycle(['cool', 'heat', 'vent', 'dry'], mode || 'dry') : mode;
+	const speed_ = () => speed = power === 'on' ?  cycle(['low', 'medium', 'high', 'auto'], speed || 'auto') : speed;
+
+	const temp_up_ = () => temp = power === 'on' ?  Math.min((temp || 23) + 1, 30) : temp;
+	const temp_down_ = () => temp = power === 'on' ?  Math.max((temp || 23) - 1, 15) : temp;
+</script>
+
+{power}
+{power === 'on' ? mode : ''}
+{power === 'on' ? speed : ''}
+{power === 'on' ? `${temp}c` : ''}
+
+<button on:click={switch_}>ON/OFF</button>
+<button on:click={mode_}>MODE</button>
+<button on:click={speed_}>FAN SPEED</button>
+<button on:click={temp_up_}>UP</button>
+<button on:click={temp_down_}>DOWN</button>
